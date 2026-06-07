507521ccbcaba74e1e7d4b8318aad18c1c349e7f
Sergey Ants <sergey594m@gmail.com>
2026-06-07 14:18:40 +0300

    [m6] add splitting logs logic

--- m6-network/src/sender/sender_client.cc
+++ m6-network/src/sender/sender_client.cc
@@ -2,11 +2,13 @@
 #include <net_logger/net_logger.h>
 
 #include "sender/grpc_message_sender.h"
+#include "common/log_message.h"
 
 #include <chrono>
 #include <format>
 #include <fstream>
 #include <iostream>
+#include <string>
 
 namespace {
 
@@ -17,9 +19,60 @@ std::string GetCurrentUTC() {
   return std::format("{:%FT%T}Z", now);
 }
 
-} // namespace
+std::string GetTargetServerString(const std::string& host,
+                                  const std::string& port) {
+  return host + std::string(":") + port;
+}
 
-int main(int argc, char **argv) {
+std::vector<std::string> SplitByLines(const std::string& text,
+                                      std::size_t lines_per_chunk) {
+  std::vector<std::string> result;
+
+  std::istringstream ss(text);
+
+  std::string line;
+  std::string chunk;
+
+  std::size_t count{0};
+
+  while (std::getline(ss, line)) {
+    chunk += line + '\n';
+    ++count;
+
+    if (count == lines_per_chunk) {
+      result.push_back(std::move(chunk));
+      chunk.clear();
+      count = 0;
+    }
+  }
+
+  if (!chunk.empty()) {
+    result.push_back(std::move(chunk));
+  }
+
+  return result;
+}
+
+std::vector<application::LogMessage> GetLogMessages(
+    const std::vector<std::string>& payloads, const std::string& log_source) {
+  std::vector<application::LogMessage> messages;
+  messages.reserve(payloads.size());
+
+  for (const auto& payload : payloads) {
+    messages.emplace_back(log_source, GetCurrentUTC(), payload);
+  }
+
+  return messages;
+}
+
+std::string GetServerNameOrLocalhost() {
+  const auto* const chars = std::getenv("SERVER_HOST");
+  return (nullptr != chars) ? chars : "localhost";
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
   if (argc != 2) {
     std::cout << "Usage: sender [path_to_file]\n";
     return 1;
@@ -43,14 +96,21 @@ int main(int argc, char **argv) {
     net::logger::ProcessStream(buffer, payload, filter);
     std::string payload_str = payload.str();
 
-    application::LogMessage msg{"ipv4_filter", GetCurrentUTC(), payload_str};
+    auto payloads = SplitByLines(payload_str, 3);
 
-    auto channel = grpc::CreateChannel("localhost:50051",
-                                       grpc::InsecureChannelCredentials());
+    std::vector<application::LogMessage> log_messages =
+        GetLogMessages(payloads, "ipv4_filter");
+
+    std::string target =
+        GetTargetServerString(GetServerNameOrLocalhost(), "50051");
+    auto channel =
+        grpc::CreateChannel(target, grpc::InsecureChannelCredentials());
     datatransfer::GrpcMessageSender sender(channel);
 
-    sender.Send(msg);
-  } catch (const std::exception &e) {
+    for (const auto& msg : log_messages) {
+      sender.Send(msg);
+    }
+  } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
     return 1;
   } catch (...) {
